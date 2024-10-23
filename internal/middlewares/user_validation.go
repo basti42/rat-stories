ebb4415e63b5bd3a2feca6348fefa260fa4ac129
Sebastian Wehner <Basti.wehner@googlemail.com>
2024-10-23 22:01:57 +0200

    feat: token verification middleware

--- internal/middlewares/user_validation.go
+++ internal/middlewares/user_validation.go
@@ -1,58 +1,91 @@
 package middlewares
 
 import (
+	"errors"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/basti42/stories-service/internal/system"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
-// func extractBearerToken(c *gin.Context) (string, error) {
-// 	header := c.GetHeader("Authorization")
-// 	if header == "" {
-// 		return "", errors.New("No 'authorization' header found")
-// 	}
-// 	tokenStrings := strings.Split(header, " ")
-// 	if len(tokenStrings) != 2 {
-// 		return "", errors.New("incorrectly formatted authorization header")
-// 	}
-// 	return tokenStrings[1], nil
-// }
+func extractBearerToken(c *gin.Context) (string, error) {
+	header := c.GetHeader("Authorization")
+	if header == "" {
+		return "", errors.New("No 'authorization' header found")
+	}
+	tokenStrings := strings.Split(header, " ")
+	if len(tokenStrings) != 2 {
+		return "", errors.New("incorrectly formatted authorization header")
+	}
+	return tokenStrings[1], nil
+}
 
-// func parseTokenString(tokenString string) (*jwt.Token, error) {
-// 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-// 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-// 			return nil, errors.New("bad signing method received")
-// 		}
-// 		return []byte(system.JWT_SECRET), nil
-// 	})
-// 	if err != nil {
-// 		return nil, errors.New("bad jwt")
-// 	}
-// 	return token, nil
-// }
+func parseTokenString(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("bad signing method received")
+		}
+		return []byte(system.JWT_SECRET), nil
+	})
+	if err != nil {
+		return nil, errors.New("bad jwt")
+	}
+	return token, nil
+}
 
 func UserValidationMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		// jwtString, err := extractBearerToken(c)
-		// if err != nil {
-		// 	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
-		// 	return
-		// }
+		jwtString, err := extractBearerToken(c)
+		if err != nil {
+			log.Printf("error extracting token: %v", err)
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
 
-		// token, err := parseTokenString(jwtString)
-		// if err != nil {
-		// 	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
-		// 	return
-		// }
+		token, err := parseTokenString(jwtString)
+		if err != nil {
+			log.Printf("error parsing token: %v", err)
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
 
-		// _, ok := token.Claims.(jwt.MapClaims)
-		// if !ok {
-		// 	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "unable to parse claims"})
-		// 	return
-		// }
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			log.Printf("error getting token claims")
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "unable to parse claims"})
+			return
+		}
 
-		// TODO extract claims and set into context
+		data := claims["data"].(map[string]any)
 
-		c.Set("user-uuid", "1858f3ab-9cb3-4b15-96b6-c4407cc7cfc7")
+		userUUIDstring, ok := data["user_uuid"].(string)
+		if !ok {
+			log.Printf("mising: user-uuid")
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "mising payload in jwt"})
+			return
+		}
+
+		subscriptionType, ok := data["subscription_type"].(string)
+		if !ok {
+			log.Printf("mising: subscription type")
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "mising payload in jwt"})
+			return
+		}
+
+		userRole, ok := data["role"].(string)
+		if !ok {
+			log.Printf("mising: user-role")
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "mising payload in jwt"})
+			return
+		}
+
+		c.Set("user-uuid", userUUIDstring)
+		c.Set("user-role", userRole)
+		c.Set("subscription-type", subscriptionType)
 		c.Next()
 	}
 }
